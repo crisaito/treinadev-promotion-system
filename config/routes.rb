c2be57e8e314abd7f3b5709da469b89f41edfa52
Cris_Saito <crhisaito@gmail.com>
2021-02-08 01:34:11 -0300

    User sign in and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :promotions, only: [:index, :show, :new, :create] do
 
+  devise_for :users
+  
+  resources :promotions, only: [:index, :show, :new, :create] do
     post 'generate_coupons', on: :member
   end
 
