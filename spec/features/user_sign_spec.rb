71de581385502f1862e723eaefd8d4b80f6bfbda
Cris_Saito <crhisaito@gmail.com>
2021-03-06 20:24:48 -0300

    test: user sign up successfully

--- spec/features/user_sign_spec.rb
+++ spec/features/user_sign_spec.rb
@@ -37,4 +37,18 @@ feature 'User sign in' do
             expect(page).to have_link 'Entrar'
         end
     end
+
+    scenario 'up successfully' do
+        visit root_path
+        click_on 'Entrar'
+        click_on 'Sign up'    
+        fill_in 'E-mail', with: 'cris@gmail.com'
+        fill_in 'Senha', with: '123456'
+        fill_in 'Confirme sua senha', with: '123456'
+        click_on 'Sign up'  
+        
+        expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
+        expect(page).to have_content 'cris@gmail.com' 
+        expect(page).to have_link 'Sair'
+    end
 end
