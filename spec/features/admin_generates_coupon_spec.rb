6e7ae72372ba2cf4a31f875a38f4126746a60a6e
Cris_Saito <crhisaito@gmail.com>
2021-03-07 22:19:26 -0300

    validate show button Gerar cupons

--- spec/features/admin_generates_coupon_spec.rb
+++ spec/features/admin_generates_coupon_spec.rb
@@ -3,22 +3,53 @@ require 'rails_helper'
 feature 'Admin generates coupons' do
     scenario 'of a promotion' do
         user = User.create!(email: 'cris@mail.com', password: '123456')
+        other_user = User.create!(email: 'tomi@mail.com', password: '123456')
         promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
                           code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
                           expiration_date: '22/12/2033', user: user)
+        promotion.approve!(other_user)
+        promotion.reload
         
-        login_as user
+        login_as other_user
         visit root_path
         click_on 'Promoções'
         click_on promotion.name
         click_on 'Gerar cupons'
 
         expect(current_path).to eq(promotion_path(promotion))
-        
         expect(page).to have_content('Cupons gerados com sucesso')
         expect(page).to have_content('NATAL10-0001')
         expect(page).to have_content('NATAL10-0100')
         expect(page).not_to have_content('NATAL10-0101')
-    
+    end
+
+    scenario 'hide button if promotion is not approved yet' do
+        user = User.create!(email: 'cris@mail.com', password: '123456')
+        promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                          code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                          expiration_date: '22/12/2033', user: user)
+
+        login_as user
+        visit promotion_path(promotion)
+                          
+        expect(promotion.approved?).to be_falsy
+        expect(page).not_to have_link('Gerar cupons')
+    end
+
+    scenario 'hide button if coupons generated' do
+        user = User.create!(email: 'cris@mail.com', password: '123456')
+        other_user = User.create!(email: 'tomi@mail.com', password: '123456')
+        promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                          code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                          expiration_date: '22/12/2033', user: user)
+        promotion.approve!(other_user)
+        promotion.reload
+        
+        login_as other_user
+        visit promotion_path(promotion)
+        click_on 'Gerar cupons'
+
+        expect(promotion.approved?).to be_truthy
+        expect(page).not_to have_link('Gerar cupons')
     end
 end
